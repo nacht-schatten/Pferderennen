0e7c7015586348542c8951c242508edff6f55c29
nacht-schatten <joh3011@hotmail.de>
2026-08-26 10:25:48 +0200

    Fix title formatting in Pferderennen.py

--- Pferderennen.py
+++ Pferderennen.py
@@ -244,7 +244,7 @@ def extract_competition_data(url):
 # ---------------------------------------------------------
 # Streamlit UI
 # ---------------------------------------------------------
-st.title("- Das Große Pferderennen! - 🌀Hengste vs 🌸Stuten")
+st.title("--- Das Große Pferderennen! --- 🌀Hengste vs 🌸Stuten")
 
 
 
